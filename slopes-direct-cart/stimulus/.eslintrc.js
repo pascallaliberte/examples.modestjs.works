fa3767622ed2becda5c31e059b798d27954353e7
Pascal Laliberté <pascal@pascallaliberte.me>
2018-12-12 14:09:30 -0500

    Start slopes-direct-cart/stimulus

---
+++ slopes-direct-cart/stimulus/.eslintrc.js
@@ -0,0 +1,13 @@
+const config = {
+  "extends": "airbnb-base",
+  "env": {
+    "browser": true
+  },
+  "rules": {
+    "semi": 0,
+    "no-trailing-spaces": 0,
+    "no-param-reassign": 0
+  }
+};
+
+module.exports = config;
