9ebc7a24899821ad0586c63b00f67d7e0935cfc4
Pascal Laliberté <pascal@pascallaliberte.me>
2018-12-13 10:43:19 -0500

    cart/spot-vue: render the cart through a vue template

--- cart/spot-vue/_src/vue/cart.vue
+++ cart/spot-vue/_src/vue/cart.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    Vue
+  <div class="container cart">
+    <div class="cart-items">
+      <div v-for="item in items" :index="item.item_name" class="row cart-item">
+        <div class="col-7">
+           {{ item.item_name }}
+        </div>
+        <div class="col-2">
+          <label class="sr-only">Quantity</label>
+          <input type="number" v-model="item.quantity" />
+        </div>
+        <div class="col-3 text-right">
+          <span class="cart-item-price">
+            <span class="cart-item-price-currency">$</span>
+            <span class="cart-item-price-subtotal">{{ item.subtotal }}</span>
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-9 text-right">
+        Subtotal
+      </div>
+      <div class="col-3 text-right">
+        <span class="cart-subtotal-price">
+          <span class="cart-subtotal-price-currency">$</span>
+          <span class="cart-subtotal-price-subtotal">{{ subtotal }}</span>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
