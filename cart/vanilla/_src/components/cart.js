49ef37dcd8ff0a03c5c0640be280cc8764ad1034
Pascal Laliberté <pascal@pascallaliberte.me>
2018-12-13 14:32:50 -0500

    cart/vanilla: satisfy linter for cart component

--- cart/vanilla/_src/components/cart.js
+++ cart/vanilla/_src/components/cart.js
@@ -1,14 +1,32 @@
-/* eslint-disable */
 const quantityFieldsSelector = '[data-behavior="cart-item-quantity"]'
 const itemSubtotalSelector = '[data-behavior="cart-item-subtotal"]'
 const cartSubtotalSelector = '[data-behavior="cart-subtotal"]'
 
-function enableQuantityFields() {
-  document.addEventListener('change', function(event) {
-    if (event.target.getAttribute('data-behavior') !== 'cart-item-quantity') return
-    updateItemSubtotal(event)
-    broadcastNewCartQuantity()
+function roundCurrency(float) {
+  // see https://stackoverflow.com/a/18358056
+  return +(`${Math.round(`${float}e+2`)}e-2`)
+}
+
+function updateCartSubtotal() {
+  const subtotalEls = document.querySelectorAll(itemSubtotalSelector)
+  const cartSubtotalEl = document.querySelector(cartSubtotalSelector)
+  
+  if (subtotalEls.length === 0) return
+  if (!cartSubtotalEl) return
+  
+  const subtotals = []
+  subtotalEls.forEach((el) => {
+    if (!el.hasAttribute('data-item-subtotal')) return
+    subtotals.push(parseFloat(el.getAttribute('data-item-subtotal')))
   })
+  
+  const subtotal = subtotals.reduce((accumulator, currentItemSubTotal) => {
+    if (currentItemSubTotal === null) return accumulator
+    return accumulator + currentItemSubTotal
+  })
+  
+  // update the cart subtotal
+  cartSubtotalEl.textContent = roundCurrency(subtotal)
 }
 
 function updateItemSubtotal(event) {
@@ -17,7 +35,7 @@ function updateItemSubtotal(event) {
   const quantity = event.target.value
   const productId = event.target.getAttribute('data-product-id')
 
-  let itemSubtotalEl = document.querySelector(`${itemSubtotalSelector}[data-product-id="${productId}"]`)
+  const itemSubtotalEl = document.querySelector(`${itemSubtotalSelector}[data-product-id="${productId}"]`)
 
   if (!itemSubtotalEl) return
   if (!itemSubtotalEl.hasAttribute('data-item-price')) return
@@ -31,49 +49,32 @@ function updateItemSubtotal(event) {
   updateCartSubtotal()
 }
 
-function updateCartSubtotal() {
-  const subtotalEls = document.querySelectorAll(itemSubtotalSelector)
-  const cartSubtotalEl = document.querySelector(cartSubtotalSelector)
-  
-  if (subtotalEls.length === 0) return
-  if (!cartSubtotalEl) return
-  
-  const subtotals = []
-  subtotalEls.forEach(function(el) {
-    if (!el.hasAttribute('data-item-subtotal')) return
-    subtotals.push(parseFloat(el.getAttribute('data-item-subtotal')))
-  })
-  
-  const subtotal = subtotals.reduce(function(accumulator, currentItemSubTotal) {
-    if (currentItemSubTotal === null) return accumulator
-    return accumulator + currentItemSubTotal
-  })
-  
-  // update the cart subtotal
-  cartSubtotalEl.textContent = roundCurrency(subtotal)
-}
-
 function broadcastNewCartQuantity() {
   const quantityFields = document.querySelectorAll(quantityFieldsSelector)
   
   if (quantityFields.length === 0) return
   
   let newQuantity = 0;
-  quantityFields.forEach(function(el) {
+  quantityFields.forEach((el) => {
     if (!el.value) return
-    newQuantity = newQuantity + parseInt(el.value)
+    newQuantity += parseInt(el.value, 10)
   })
   
-  const event = new CustomEvent('cart-quantity-updated', { detail: {
-    newQuantity: newQuantity
-  }});
+  const event = new CustomEvent('cart-quantity-updated', { 
+    detail: {
+      newQuantity,
+    },
+  });
   
   document.dispatchEvent(event)
 }
 
-function roundCurrency(float) {
-  // see https://stackoverflow.com/a/18358056
-  return +(Math.round(float + "e+2")  + "e-2")
+function enableQuantityFields() {
+  document.addEventListener('change', (event) => {
+    if (event.target.getAttribute('data-behavior') !== 'cart-item-quantity') return
+    updateItemSubtotal(event)
+    broadcastNewCartQuantity()
+  })
 }
 
 enableQuantityFields()
